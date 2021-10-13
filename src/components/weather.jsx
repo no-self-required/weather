bc6d60eaa319c5b475c576514d7888327291d7e6
no-self-required <louism.coding@gmail.com>
2021-10-12 21:12:20 -0400

    setup app

---
+++ src/components/weather.jsx
@@ -0,0 +1,11 @@
+const key = process.env.owKey 
+
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+export default function Weather() {
+  return (
+    <div className="weathercomp">
+      weathercomp
+    </div>
+  );
+}

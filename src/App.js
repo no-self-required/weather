a0d6f8ba1fa34d13724e7a124673fdf28214b2d4
no-self-required <louism.coding@gmail.com>
2021-10-14 19:26:12 -0400

    fix env

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import './App.css';
-import Weather from './components/weather'
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Weather from "./components/weather";
+import axios from "axios";
+
+const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apiKey}`;
 
 function App() {
+
+  axios
+    .get(apiUrl)
+    .then(function (response) {
+      console.log(response)
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
   return (
     <div className="navbar">
       weather
--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,6 +1,6 @@
 const key = process.env.owKey 
 
-//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
 
 export default function Weather() {
   return (

2b89bb7a9ae847f798fafdbe426ade4fde37ac7d
no-self-required <louism.coding@gmail.com>
2021-10-15 21:05:40 -0400

    add search function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-import Searchbox from "./components/searchbox";
-import axios from "axios";
+// import Searchbox from "./components/searchbox";
+// import axios from "axios";
 
 const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apiKey}`;
+const apiBaseUrl = `https://api.openweathermap.org/data/2.5/`;
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
 
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${apiBaseUrl}weather?q=${query}&appid=${apiKey}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setQuery('');
+          setWeather(result);
+          console.log(result);
+        });
+    }
+  };
   const datebuilder = (d) => {
     let months = [
       "January",
@@ -37,24 +50,24 @@ function App() {
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
-    let year  = d.getFullYear();
+    let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`
+    return `${day} ${date} ${month} ${year}`;
   };
-
-  axios
-    .get(apiUrl)
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
 
   return (
     <div className="app">
       <main>
-        <Searchbox></Searchbox>
+        <div className="searchbox">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Enter a city"
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          ></input>
+        </div>
         <div className="location-time">
           <div className="location">Toronto</div>
           <div className="date">{datebuilder(new Date())}</div>
@@ -65,3 +78,12 @@ function App() {
 }
 
 export default App;
+
+// axios
+// .get(apiUrl)
+// .then(function (response) {
+//   console.log(response);
+// })
+// .catch(function (error) {
+//   console.error(error);
+// });
